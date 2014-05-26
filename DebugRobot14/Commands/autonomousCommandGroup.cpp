2c7a601bd933469f93c548cc34d396f003a9c8f9
Willster419 <willster419@outlook.com>
2014-05-25 21:58:50 -0400

    auto debugging start

---
+++ DebugRobot14/Commands/autonomousCommandGroup.cpp
@@ -0,0 +1,25 @@
+/* FIRST Team 1073's RobotBuilder (0.0.2) for WPILibExtensions ---
+Do not mix this code with any other version of RobotBuilder! */
+
+
+
+#include "autonomousCommandGroup.h"
+
+autonomousCommandGroup::autonomousCommandGroup() {
+	// Add Commands here:
+	// e.g. AddSequential(new Command1());
+	//      AddSequential(new Command2());
+	// these will run in order.
+
+	// To run multiple commands at the same time,
+	// use AddParallel()
+	// e.g. AddParallel(new Command1());
+	//      AddSequential(new Command2());
+	// Command1 and Command2 will run in parallel.
+
+	// A command group will require all of the subsystems that each member
+	// would require.
+	// e.g. if Command1 requires chassis, and Command2 requires arm,
+	// a CommandGroup containing them would require both the chassis and the
+	// arm.
+}
---
+++ DebugRobot14/Commands/autonomousCommandGroup.h
@@ -0,0 +1,21 @@
+/* FIRST Team 1073's RobotBuilder (0.0.2) for WPILibExtensions ---
+Do not mix this code with any other version of RobotBuilder! */
+
+
+
+#ifndef AUTONOMOUSCOMMANDGROUP_H
+#define AUTONOMOUSCOMMANDGROUP_H
+
+#include "Commands/CommandGroup.h"
+
+/**
+ *
+ *
+ * @author ExampleAuthor
+ */
+class autonomousCommandGroup: public CommandGroup {
+public:	
+	autonomousCommandGroup();
+};
+
+#endif
