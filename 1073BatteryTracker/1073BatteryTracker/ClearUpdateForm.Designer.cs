64f2e7d9e0523440d8e945bcbf65ac2106a9a8bc
Willster419 <willster419@outlook.com>
2014-10-11 22:58:44 -0400

    made the font a little bigger

--- 1073BatteryTracker/1073BatteryTracker/ClearUpdateForm.Designer.cs
+++ 1073BatteryTracker/1073BatteryTracker/ClearUpdateForm.Designer.cs
@@ -34,9 +34,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(59, 9);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 5);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(132, 13);
+            this.label1.Size = new System.Drawing.Size(194, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Please wait, updating list...";
             // 
